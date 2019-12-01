b6781a7ef69b42e2077848be5e27753c0865972b
Matthew Brett <matthew.brett@gmail.com>
2019-12-01 18:08:03 +0000

    Working version of nb build script

--- scripts/build_nb_book.R
+++ scripts/build_nb_book.R
@@ -1,47 +1,52 @@
 #!/usr/bin/env Rscript
-library("optparse")
 
-option_list <- list(
-  make_option(c("-e", "--edition"), type="character", default=NULL,
+opt_list <- list(
+  optparse::make_option(c("-e", "--edition"),
+                        type="character",
+                        default=NULL,
               help="edition (python or r)",
               metavar="character"),
-  make_option(c("-d", "--out-dir"), type="character", default=NULL,
+  optparse::make_option(c("-o", "--out-dir"),
+                        type="character",
+                        default=NULL,
               help="output directory",
               dest='out_dir',
               metavar="character")
 )
 
-opt_parser <- OptionParser(option_list=option_list)
-opt <- parse_args(opt_parser)
+opt_parser <- optparse::OptionParser(option_list=opt_list)
+opt__ <- optparse::parse_args(opt_parser)
 
-if (is.null(opt$edition)){
+if (is.null(opt__$edition)){
   print_help(opt_parser)
   stop("Specify edition", call.=FALSE)
 }
-opt$edition <- tolower(opt$edition)
-if (!is.element(opt$edition, c('python', 'r'))) {
+opt__$edition <- tolower(opt__$edition)
+if (!is.element(opt__$edition, c('python', 'r'))) {
   print_help(opt_parser)
-  stop("out-dir must be 'python' or 'r'", call.=FALSE)
+  stop("edition must be 'python' or 'r'", call.=FALSE)
 }
-if (is.null(opt$out_dir)){
+if (is.null(opt__$out_dir)){
   print_help(opt_parser)
   stop("Specify output directory", call.=FALSE)
 }
 
 # Signal notebook version to _common.R
-Sys.setenv(BOOK_ED=stringr::str_interp('${opt$edition}_nb'))
+Sys.setenv(BOOK_ED=stringr::str_interp('${opt__$edition}-nb'))
 # Configuration file for build.
-val <- stringr::str_interp('_${opt$edition}_bookdown.yml')
+opt__$config <- stringr::str_interp('_${opt__$edition}_bookdown.yml')
 # Markdown output config.
 # See: https://github.com/rstudio/bookdown/issues/782
-fmt <- bookdown::markdown_document2(base_format=rmarkdown::md_document)
+opt__$fmt <- bookdown::markdown_document2(base_format=rmarkdown::md_document)
 # Turn off citation links.
-fmt$pandoc$args <- c(fmt$pandoc$args,
+opt__$fmt$pandoc$args <- c(opt__$fmt$pandoc$args,
                      '--metadata',
                      'link-citations=no')
 # Build single Markdown output document.
-# This command raises an error - see:
-# https://github.com/rstudio/bookdown/issues/835
-bookdown::render_book('index.Rmd', fmt,
-                      config_file=val,
-                      output_dir=opt$out_dir)
+# See https://github.com/rstudio/bookdown/issues/835
+# for explanation of clean_envir
+rm(opt_list, opt_parser)
+bookdown::render_book('index.Rmd', opt__$fmt,
+                      clean_envir=FALSE,
+                      config_file=opt__$config,
+                      output_dir=opt__$out_dir)
