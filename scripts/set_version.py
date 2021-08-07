0cdf9a272d1c1751b2901ce25ba0421454e962fc
Matthew Brett <matthew.brett@gmail.com>
2021-08-07 23:15:38 +0100

    Refactor to use variables

--- scripts/set_version.py
+++ scripts/set_version.py
@@ -4,7 +4,8 @@
 
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
 
-TEMPLATE_FNAME = '_quarto.yml.template'
+QUARTO_TEMPLATE = '_quarto.yml.template'
+VARS_TEMPLATE = '_variables.yml.template'
 
 def get_parser():
     parser = ArgumentParser(description=__doc__,  # Usage from docstring
@@ -16,10 +17,14 @@ def get_parser():
 
 def set_version(version):
     version = version.lower()
-    with open(TEMPLATE_FNAME, 'rt') as fobj:
-        fmt_str = fobj.read()
-    with open("_quarto.yml", 'wt') as fobj:
-        fobj.write(fmt_str.format(version=version))
+    language = version.capitalize()
+    other_language = 'Python' if language == 'R' else 'R'
+    for fname in (QUARTO_TEMPLATE, VARS_TEMPLATE):
+        with open(fname, 'rt') as fobj:
+            fmt_str = fobj.read()
+        out_fname = fname.replace('.template', '')
+        with open(out_fname, 'wt') as fobj:
+            fobj.write(fmt_str.format(**locals()))
 
 
 def main():
