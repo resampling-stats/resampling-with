b71150c3bcd25ad41aee2570e04b1f5f2bd5121e
Matthew Brett <matthew.brett@gmail.com>
2021-08-09 21:56:19 +0100

    Strip out fancy footwork in R for markup

--- source/_common.R
+++ source/_common.R
@@ -17,47 +17,13 @@ options(dplyr.print_min = 6, dplyr.print_max = 6)
 
 # Book edition from build spec
 ._spec <- yaml::yaml.load_file(input = '_variables.yml')
-book_edition <- ._spec$edition
-
-# Can be notebook edition, as in 'python-nb'
-._parts <- strsplit(book_edition, '-')[[1]]
-book_edition <-._parts[1]
-._nb_part <- ._parts[2]
-is_nb <- if (!is.na(._nb_part) & ._nb_part == 'nb') TRUE else FALSE
-
-other_edition <- if (book_edition == 'python') 'r' else 'python'
+book_edition <- tolower(._spec$edition)
 
 is_r_ed <- book_edition == 'r'
 is_py_ed <- book_edition == 'python'
-eval_r_ed = is_r_ed & !is_nb
-eval_py_ed = is_py_ed & !is_nb
-
-# For conditional inclusion of text blocks.
-comment_start <- "<!---"
-comment_end <- "-->"
-begin_ispy <- function () { return ( if (!is_py_ed) { comment_start } ) }
-end_ispy <- function () { return ( if (!is_py_ed) { comment_end } )  }
-begin_isr <- function () { return ( if (!is_r_ed) { comment_start } ) }
-end_isr <- function () { return ( if (!is_r_ed) { comment_end } ) }
-begin_isnb <- function (nb_fname) { return ( if (!is_nb) { comment_start } ) }
-end_isnb <- function () { return ( if (!is_nb) { comment_end } ) }
-py_or_r <- function(is_py, is_r, backticks=TRUE) {
-    result <- if (is_py_ed) is_py else is_r
-    if (backticks) stringr::str_interp("`${result}`") else result
-}
-
-# Language for use inside Markdown text.
-# Use with:
-#  Now some code in the `r book_lang` language.
-book_lang <- paste(toupper(substring(book_edition, 1,1)),
-                   substring(book_edition, 2), sep="")
-other_lang <- paste(toupper(substring(other_edition, 1,1)),
-                   substring(other_edition, 2), sep="")
-# Shortcut, it's boring to type.
-BL <- book_lang
 
 knitr::opts_template$set(r_ed = list(eval=is_r_ed, echo=is_r_ed),
-                         py_ed = list(eval=eval_py_ed, echo=is_py_ed))
+                         py_ed = list(eval=is_py_ed, echo=is_py_ed))
 
 # For Python code.
 library(reticulate)
