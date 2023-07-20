a75498a2c9f6e3c953c41ff3f25d4eae15eae482
Stefan van der Walt <stefanv@berkeley.edu>
2023-07-20 12:10:45 -0700

    Use NULL instead of NA

--- source/_common.R
+++ source/_common.R
@@ -17,8 +17,8 @@ knitr::opts_chunk$set(
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold",
-  eval = NA,
-  echo = NA
+  eval = NULL,
+  echo = NULL
   # Do not enable Reticulate jupyter_compat mode for now, see:
   # https://github.com/rstudio/reticulate/issues/1387
   # jupyter_compat = TRUE  # Reticulate enable - only show repr for last expr
@@ -82,13 +82,13 @@ get_var = function(name) {
 # See: https://stackoverflow.com/questions/76732162/is-there-a-way-to-detect-the-chunk-language-when-setting-knitr-chunk-options
 knitr::opts_hooks$set(
   echo = function(options) {
-    if (!is.na(options$echo)) {
+    if (!is.null(options$echo)) {
       options$echo <- (options$engine == "python" & is_py_ed) | (options$engine == "R" & is_r_ed)
     }
     return (options)
   },
   eval = function(options) {
-    if (!is.na(options$eval)) {
+    if (!is.null(options$eval)) {
       options$eval <- (options$engine == "python" & is_py_ed) | (options$engine == "R" & is_r_ed)
     }
     return (options)
