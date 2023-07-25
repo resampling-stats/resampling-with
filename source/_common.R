700fc539eab6e6fe16d695d31c49c893a2018171
Matthew Brett <matthew.brett@gmail.com>
2023-07-25 17:06:07 +0100

    Simplify logic of option defaults
    Follow suggestion at original StackOverflow thread, and just check for
    not-logical prior values, to see if default has been changed.  I _think_
    that the values will be NULL if not set, but this just checks if they
    are TRUE or FALSE, to check if they are set.

--- source/_common.R
+++ source/_common.R
@@ -17,11 +17,21 @@ knitr::opts_chunk$set(
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
   fig.show = "hold",
+<<<<<<< Updated upstream
   eval = NA,
   echo = NA
   # Do not enable Reticulate jupyter_compat mode for now, see:
   # https://github.com/rstudio/reticulate/issues/1387
   # jupyter_compat = TRUE  # Reticulate enable - only show repr for last expr
+||||||| Stash base
+  eval = NA,
+  echo = NA,
+  # See: https://github.com/rstudio/reticulate/issues/1387
+  jupyter_compat = TRUE  # Reticulate enable - only show repr for last expr
+=======
+  # See: https://github.com/rstudio/reticulate/issues/1387
+  jupyter_compat = TRUE  # Reticulate enable - only show repr for last expr
+>>>>>>> Stashed changes
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
@@ -81,7 +91,7 @@ get_var = function(name) {
 
 # See: https://stackoverflow.com/questions/76732162/is-there-a-way-to-detect-the-chunk-language-when-setting-knitr-chunk-options
 set_true_for_matching_lang <- function(options, field) {
-    if (is.na(options[[field]])) {
+    if (!is.logical(options[[field]])) {
       options[[field]] <- (tolower(options$engine) == ._lang)
     }
     return (options)
