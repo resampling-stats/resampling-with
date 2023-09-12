4a1b9726f85d23586802e04f8d591e706cf269d7
Matthew Brett <matthew.brett@gmail.com>
2023-09-12 09:13:53 +0100

    Refactor data-generation scripts

--- source/data/congress.py
+++ source/data/congress.py
@@ -12,4 +12,4 @@ GITHUB_DATASETS = 'https://raw.githubusercontent.com/odsti/datasets/main/'
 url = GITHUB_DATASETS + 'congress_118/processed/congress_118.csv'
 df = pd.read_csv(url).sort_values('Median_Income')
 df.insert(0, 'Ascending_Rank', range(1, len(df) + 1))
-df.to_csv(HERE / 'congress_districts.csv', index=None)
+df.to_csv(HERE / 'congress_2023.csv', index=None)
