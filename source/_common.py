08be8b8f0885b8580df28849b19970529a615bb7
Matthew Brett <matthew.brett@gmail.com>
2024-08-13 13:48:00 +0100

    Add section on or / and

--- source/_common.py
+++ source/_common.py
@@ -6,16 +6,20 @@ import numpy.random as _npr
 _default_rng = _npr.default_rng
 
 
-def _default_seeded_rng(*args, **kwargs):
-    """ Use original default_rng if any arguments, else use given seed
-    """
-    if args or kwargs:
-        return _default_rng(*args, **kwargs)
-    # Seed injected from _common.R script.
-    return _default_rng(_QUARTO_SEED)
+def _make_default_seeded_rng(default_seed):
+
+    def dsrng(*args, **kwargs):
+        """ Use original default_rng if any arguments, else use given seed
+        """
+        if args or kwargs:
+            return _default_rng(*args, **kwargs)
+        # Seed injected from _common.R script.
+        return _default_rng(default_seed)
+
+    return dsrng
 
 
-_npr.default_rng = _default_seeded_rng
+_npr.default_rng = _make_default_seeded_rng(_QUARTO_SEED)
 
 
 # Python variable in Python edition else R variable.
